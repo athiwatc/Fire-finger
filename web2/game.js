dfca43258bbfb83d29f4084d6396df07644e5be1
Thanakorn <stylus_l@hotmail.com>
2012-11-06 18:56:26 +0700

    add text menu

--- web2/game.js
+++ web2/game.js
@@ -1,24 +1,47 @@
 window.onload = function () {
-	var width = 600;
-	var height = 300;
+	var width = 360;
+	var height = 640;
 
     Crafty.init(width, height);
 	Crafty.background('rgb(127,127,127)');
 
 	Crafty.scene("Start",function(){
 
-	})
+		Crafty.e("Game,2D,Canvas,Text")
+			.text("FireFinger")
+			.textColor("#ffffff")
+			.textFont({
+				size: "30px",
+				weight: 'bold',
+				family: "Segoe UI"
+			})
+			.attr({
+				x: width/2,
+				y: height/3
+			});
+
+		Crafty.e("Single,2D,Canvas,Text")
+			.text("SinglePlayer")
+			.textColor("#ffffff")
+			.textFont({size : "20px",family:"Segoe UI"})
+			.attr({
+				x: width/2,
+				y: height/3 + 100
+			});
+
+	});
 
 	Crafty.scene("Single",function(){
 
-	})
+	});
 
 	Crafty.scene("Multi",function(){
 
-	})
+	});
 
 	Crafty.scene("Credit",function(){
-		
-	})
 
+	});
+
+	Crafty.scene("Start");
 };
