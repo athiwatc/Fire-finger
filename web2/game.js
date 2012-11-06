c21118e6e5b23af812990fb3995d08c9b3a57cd1
Thanakorn <stylus_l@hotmail.com>
2012-11-06 18:10:27 +0700

    Create Scene

--- web2/game.js
+++ web2/game.js
@@ -2,4 +2,20 @@ window.onload = function () {
     Crafty.init(600, 300);
 	Crafty.background('rgb(127,127,127)');
 
+	Crafty.scene("Start",function(){
+
+	})
+
+	Crafty.scene("Single",function(){
+
+	})
+
+	Crafty.scene("Multi",function(){
+
+	})
+
+	Crafty.scene("Credit",function(){
+		
+	})
+
 };
